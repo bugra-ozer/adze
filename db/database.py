6ca92808f72b5b04e2516f617a7006e6e9bce334
bugra-ozer <bugraozer791@gmail.com>
2026-08-15 13:36:54 +0100

    feat: add db/ ORM and SQLAlchemy modules

---
+++ db/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from common import constants as con
+from pathlib import Path
+import os
+
+DATABASE_URL=os.getenv(con.DB_URL_KEY)
+if DATABASE_URL is None: DATABASE_URL=f'sqlite:///{Path(__file__).parent.parent / con.DB_FALLBACK_FOLDER / con.DB_FALLBACK_FILE}'
+Engine=create_engine(DATABASE_URL)
+Session=sessionmaker(bind=Engine)
+Base=declarative_base()
