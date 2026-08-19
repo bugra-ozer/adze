cc66f02d2103887df7490e3b5115e59ef53e8d12
bugra-ozer <bugraozer791@gmail.com>
2026-08-19 13:41:26 +0100

    task: update github normalization

--- providers/github.py
+++ providers/github.py
@@ -1,16 +1,16 @@
-from common.verify import signature_verify
+from common.verifier import signature_verify
 from main import get_secret
-from common import constants as cons
+from common import constants as con
 from datetime import datetime, timezone
 import json
 
 def handle_event(body_bytes, headers)-> dict | False:
     """Verify signature, and return normalized packet."""
     msg = body_bytes
-    signature_key = cons.KEY_GH_SIGNATURE
-    prefix = cons.PREFIX_GH
-    digest_mod = cons.DIGEST_MOD_GH
-    webhook_secret_key = get_secret(cons.WEBHOOK_SECRET_KEY_GH)
+    signature_key = con.KEY_GH_SIGNATURE
+    prefix = con.PREFIX_GH
+    digest_mod = con.DIGEST_MOD_GH
+    webhook_secret_key = get_secret(con.WEBHOOK_SECRET_KEY_GH)
     if signature_verify(webhook_secret_key, signature_key, msg, digest_mod, prefix, headers):
         return normalize(body_bytes, headers)
     else:
@@ -20,12 +20,12 @@ def normalize(body_bytes, headers):
     """Standardize keys, attach timestamp, and unpack bytes body."""
     try:
         envelope={
-        cons.NORM_KEY_PROVIDER: cons.PROVIDER_GH,
-        cons.NORM_KEY_EVENT_TYPE: headers.get(cons.KEY_GH_EVENT),
-        cons.NORM_KEY_DELIVERY_ID: headers.get(cons.KEY_GH_DELIVERY),
-        cons.NORM_KEY_TIMESTAMP: datetime.now(timezone.utc).isoformat(),
-        cons.NORM_KEY_RAW_PAYLOAD: json.loads(body_bytes)
+        con.NORM_KEY_PROVIDER: con.PROVIDER_GH,
+        con.NORM_KEY_EVENT_TYPE: headers.get(con.KEY_GH_EVENT),
+        con.NORM_KEY_DELIVERY_ID: headers.get(con.KEY_GH_DELIVERY),
+        con.NORM_KEY_TIMESTAMP: datetime.now(timezone.utc).isoformat(),
+        con.NORM_KEY_RAW_PAYLOAD: json.loads(body_bytes)
         }
         return envelope
-    except ValueError: return ValueError(cons.ERROR_FAILED_PARSE)
+    except ValueError: return ValueError(con.ERROR_FAILED_PARSE)
 
---
+++ providers/twilio.py
@@ -0,0 +1,3 @@
+#TO DO:
+#1. MSG parameter needs to be constructed here. Unlike gh, twilio uses URL + post params hashed. Before calling generic signature verify, compute msg argument.
+#2. Twilio signature is base64. Needs again constructed to be hex.
